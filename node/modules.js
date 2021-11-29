97b90f44b37f5943dcab1d1047ea975831d1e9ed
aedorado <annurag94@gmail.com>
2021-11-29 22:50:03 +0530

    Create React App

---
+++ node/modules.js
@@ -0,0 +1,19 @@
+// small programs
+// bigger and bigger and bigger
+// ...
+// .. 
+// ..
+// .. 
+// .. 
+// ..
+// ..
+// Canva, YT, FB, Notion, ...
+
+// split ia large program
+// into smaller modules
+// module exports the necessary items
+// import and use what is needed from that module
+
+import {shapeName, area} from './shapes';
+
+let ar = area(10);
---
+++ node/shapes.js
@@ -0,0 +1,10 @@
+export const shapeName = 'square';
+
+export function area(a) {
+    return a * a;
+}
+
+
+//
+// we can define a default fucntion
+// that is exported from the module
