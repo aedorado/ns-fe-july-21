1aa3cbae31ca0588bfe68a8d3a0c2141cec17460
aedorado <annurag94@gmail.com>
2021-12-01 22:40:10 +0530

    Props

--- react/ns-react/src/App.js
+++ react/ns-react/src/App.js
@@ -1,15 +1,72 @@
 import './App.css';
 
-function App() {
+function Welcome(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          HI guys
-        </p>
-      </header>
+    <h1>Hello, {props.name}.</h1>
+  );
+}
+
+function UserImage(props) {
+  console.log("UserImage", props);
+  return <div className="user-image-container">
+    <img className="user-image" src={props.img.src} alt={props.img.alt} />
+  </div>
+}
+
+function UserInfo(props) {
+  console.log("UserInfo", props);
+  return (
+    <div className="info">
+      <UserImage img={props.img} />
+      <div className="user-name">{props.name}</div>
     </div>
   );
 }
 
+function CommentDesc(props) {
+  return (
+    <div>{props.description}</div>
+  );
+}
+
+function CommentTime(props) {
+  return (
+    <div>Commented @ {new Date().toLocaleTimeString()}</div>
+  );
+}
+
+function Comment(props) {
+  console.log("Comment", props);
+  return (
+    <div className="comment">
+      <UserInfo img={props.img} name={props.name}  />
+      <CommentDesc description={props.description} />
+      <CommentTime time={new Date().toLocaleTimeString()} />
+    </div>
+  );
+}
+
+// Composing compenents - A component may use other components inside of itself.
+function App() {
+  let img = {
+    src: 'https://i.pinimg.com/originals/1e/3e/9e/1e3e9ebab9ae9154a33fefcf937d2796.jpg',
+    alt: 'Sunset'
+  }
+  return (
+    <Comment img={img} name="Anurag" description="This describes you" />
+  );
+}
+
 export default App;
+
+// props are readonly
+// pure funtions - that makes no change to its arguments - no side effects
+function sum(a, b) {
+  a = 10;
+  return a + b;
+}
+// impure functions - that makes a permanent change to its arguments
+// which reflects even outside the function
+function withdraw(account, amount) {
+  account.total -= amount;
+}
--- react/ns-react/src/index.js
+++ react/ns-react/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-let h1 = React.createElement(
-  "h1", // type of element
-  { style: { color:"blue", fontFamily: 'Tahoma' }, className: "heading", id: "heading-1" }, // attributes
-  "Hello World" // content/children
-);
+// let h1 = React.createElement(
+//   "h1", // type of element
+//   { style: { color:"blue", fontFamily: 'Tahoma' }, className: "heading", id: "heading-1" }, // attributes
+//   "Hello World" // content/children
+// );
 
 // let para = React.createElement(
 //   "p",
@@ -15,52 +15,52 @@ let h1 = React.createElement(
 //   "Lorem Ipsum"
 // );
 
-let div = React.createElement(
-  "div",
-  { className: "my-div" },
-  React.createElement(
-    "ul",
-    null,
-    React.createElement("li", null, "Oranges"),
-    React.createElement("li", null, "Pear"),
-    React.createElement("li", null, "Chikoo"),
-    React.createElement("li", {style: {color: "yellow"}}, "Mango"),
-  )
-)
+// let div = React.createElement(
+//   "div",
+//   { className: "my-div" },
+//   React.createElement(
+//     "ul",
+//     null,
+//     React.createElement("li", null, "Oranges"),
+//     React.createElement("li", null, "Pear"),
+//     React.createElement("li", null, "Chikoo"),
+//     React.createElement("li", {style: {color: "yellow"}}, "Mango"),
+//   )
+// )
 
 // MVC - model (data), view (ui), controller (interaction)
 
 // JSX - Javascript Syntax Extension
 // JSX is used to declare/describe the UI of a component/element
-const firstName = "Anurag";
-const lastName = "Das";
+// const firstName = "Anurag";
+// const lastName = "Das";
 // const element = <h1>Hello, {name}!</h1>;
 // const element = <h1>Hello, {1 + 2}!</h1>;
 
-const user = {
-  firstName: 'Anurag',
-  lastName: 'Das',
-  id: 'user-id'
-}
+// const user = {
+//   firstName: 'Anurag',
+//   lastName: 'Das',
+//   id: 'user-id'
+// }
 
-function formatName(firstName, lastName) {
-  return firstName + ' ' + lastName;
-}
+// function formatName(firstName, lastName) {
+//   return firstName + ' ' + lastName;
+// }
 
-function formatNameForUser(user) {
-  return user.firstName + ' ' + user.lastName;
-}
+// function formatNameForUser(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
 
-function getGreeting(user) {
-  if (user) {
-    const ele = <div>
-      <h1 id={user.id}>Hello, {formatNameForUser(user)}</h1>
-      <h2>Element</h2>
-    </div>;
-    return ele;
-  }
-  return <h1>Hello Dr. Strange</h1>;
-}
+// function getGreeting(user) {
+//   if (user) {
+//     const ele = <div>
+//       <h1 id={user.id}>Hello, {formatNameForUser(user)}</h1>
+//       <h2>Element</h2>
+//     </div>;
+//     return ele;
+//   }
+//   return <h1>Hello Dr. Strange</h1>;
+// }
 // const element = getGreeting(user);
 // const element = <h1> Hello, {formatNameForUser(user)}! </h1>;
 // const element = <h1> Hello, {formatName(firstName, lastName)}! </h1>;
@@ -78,17 +78,40 @@ function getGreeting(user) {
 // 1. Element/component to be rendered
 // 2. Where to render it
 
-function tick() {
-  let date = new Date();
-  date = date.toLocaleTimeString();
-  ReactDOM.render(
-    <div>
-      <h1>Hello world</h1>
-      <h2>Time is {date}</h2>
-    </div>,
-    document.getElementById('root'),
-  );
-}
+// function tick() {
+//   let date = new Date();
+//   date = date.toLocaleTimeString();
+//   ReactDOM.render(
+//     <div>
+//       <h1>Hello world</h1>
+//       <h2>Time is {date}</h2>
+//     </div>,
+//     document.getElementById('root'),
+//   );
+// }
 
-setInterval(tick, 1000);
+// setInterval(tick, 1000);
+
+// REACT CLASS - COMPONENTS
+// Componets allow us to split the UI
+// into independent, resuable pieces
+// We can work with each piece in isoloation
+
+// Functions
+// Class
+
+// Props - are the input supplied to components
+// these are properties that may affect the working of the component
+
+// <div class="div-1" id="" >
+
+ReactDOM.render(
+  <App />, // Welcome({ name: "Anurag", age:"25" });
+  document.getElementById('root')
+);
+
+
+
+
+
 
